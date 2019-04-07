60ed276ae2892521656e37e38cfa63d1fac0ecf1
Liran Tal <liran.tal@gmail.com>
2019-04-07 11:16:17 +0300

    feat: barebones starting point

--- app.js
+++ app.js
@@ -3,7 +3,6 @@ const cookieParser = require('cookie-parser')
 const logger = require('morgan')
 
 const usersRouter = require('./routes/users')
-const moviesRouter = require('./routes/moviesRouter')
 
 if (process.env.DB_SEED) {
   console.log('+ Seeding DB data')
@@ -24,6 +23,5 @@ app.use(express.urlencoded({ extended: false }))
 app.use(cookieParser())
 
 app.use('/users', usersRouter)
-app.use('/movies', moviesRouter)
 
 module.exports = app
